897f971c60c529e67140fdee2e6279fb29477adf
Mathijs Jonker <mathijsjonker@msn.com>
2012-05-10 12:11:36 +0200

    Getallen opslaan als string

--- Resources/src/methods/settings.js
+++ Resources/src/methods/settings.js
@@ -21,11 +21,9 @@
 	else if (mapType === 'satelite') { sSatelite.hasCheck = true; mapView.mapType = Titanium.Map.SATELLITE_TYPE; }
 	else { sHybrid.hasCheck = true; mapView.mapType = Titanium.Map.HYBRID_TYPE; } // Niet met een else if omdat er aan het begin al gechecked wordt
 
-	// Hoogte van een boot ophalen
-	var hoogte = Titanium.App.Properties.getInt('height', 0);
-	if(hoogte !== 0) { heightField.value = hoogte; } // indien dit niet gelijk is aan 0 (standaard waarden) dan laten zien
-	var widthvalue = Titanium.App.Properties.getInt('width', 0);
-	if(widthvalue !== 0) { widthField.value = widthvalue; }	// indien dit niet gelijk is aan 0 (standaard waarden) dan laten zien
+	// Hoogte van een boot ophalen en in het tekstvakje zetten
+	heightField.value = Titanium.App.Properties.getString('height', 0);
+	widthField.value = Titanium.App.Properties.getString('width', 0);
 })();
 
 
@@ -66,13 +64,13 @@ sSatelite.addEventListener('click', function(){
 SettingsWindow.addEventListener('click', function(e){
 	heightField.blur(); 	// Verplichten om dan focus te verlieze
 	widthField.blur(); 		// idem
-	var rExp  = /[0-9]+/; // Regualar expression die test voor juiste getallen
+	var rExp  = /[0-9]+(\.[0-9]+)?/; // Regualar expression die test voor juiste getallen
 	if(heightField.value === '' || rExp.test(heightField.value)) // kijken of er een juist iets is ingevuld
 	{
 		var temp = rExp.exec(heightField.value);				// kijken of er een getal uit de regular expression komt
-		if(heightField.value === '') { Titanium.App.Properties.setInt( 'height', 0); }	// indien het leeg is dan 0 oplsaan
+		if(heightField.value === '') { Titanium.App.Properties.setString( 'height', null); }	// indien het leeg is dan 0 oplsaan
 		else {
-			Titanium.App.Properties.setInt('height', temp[0]);	// anders het eerste getal uit de regular expression opslaan
+			Titanium.App.Properties.setString('height', temp[0]);	// anders het eerste getal uit de regular expression opslaan
 			heightField.value = temp[0];						// deze waarde ook weer terug zetten
 		}
 	}
@@ -80,30 +78,25 @@ SettingsWindow.addEventListener('click', function(e){
 	if(widthField.value === '' || rExp.test(widthField.value))
 	{
 		var temp = rExp.exec(widthField.value);
-		if(widthField.value === '') { Titanium.App.Properties.setInt( 'width', 0); }
+		if(widthField.value === '') { Titanium.App.Properties.setString( 'width', null); }
 		else {
-			Titanium.App.Properties.setInt('width', temp[0]);
+			Titanium.App.Properties.setString('width', temp[0]);
 			widthField.value = temp[0];
 		}
 	}
 });
 
-/*
- * 	Twee functies die op het moment dat de tekstvelden worden aangeklikt een 
- * 	notificatie laten zien.
- */
 heightField.addEventListener('focus', function(){
-	// Nieuwe notificatie maken
 	var toast = Titanium.UI.createNotification({
-    duration: Titanium.UI.NOTIFICATION_DURATION_LONG,
-    message: 'Getallen worden afgekapt. 10.7 wordt 10. Houd hier rekening mee'
-    });
-    toast.show();
+		duration: Titanium.UI.NOTIFICATION_DURATION_LONG,
+		message: 'Decimale getallen scheiden met een .'
+	});
+	toast.show();
 });
 widthField.addEventListener('focus', function(){
 	var toast = Titanium.UI.createNotification({
-    duration: Titanium.UI.NOTIFICATION_DURATION_LONG,
-    message: 'Getallen worden afgekapt. 10.7 wordt 10. Houd hier rekening mee'
-    });
-    toast.show();
+		duration: Titanium.UI.NOTIFICATION_DURATION_LONG,
+		message: 'Decimale getallen scheiden met een .'
+	});
+	toast.show();
 });
